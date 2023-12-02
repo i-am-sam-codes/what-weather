6d1cb0ad36fe63cfa279dd807207685499394efd
i-am-sam-codes <sammmceja@gmail.com>
2023-12-01 21:49:44 -0800

    add weather display

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ function init() {
 async function fetchData(cityName) {
   try {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=a850e970fcee14c3dc6dd045b4e06dd9`
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=a850e970fcee14c3dc6dd045b4e06dd9&units=imperial`
     );
     const data = await response.json();
 
-    return data;
+    const answer = Math.floor(data.main.temp);
+
+    displayWeather(answer);
+
+    return answer;
   } catch (error) {
     console.error("Error fetching data:", error);
   }
@@ -23,6 +27,12 @@ function searchInput(e) {
   const cityName = input.value.toLowerCase();
 
   fetchData(cityName);
+}
+
+function displayWeather(answer) {
+  const display = document.querySelector(".display");
+  
+  display.textContent = "The current weather is: " + answer;
 }
 
 init();
